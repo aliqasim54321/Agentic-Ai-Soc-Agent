040b64f7138fbc975b9e30ba95f0f52eff5a78a0
Syed Ali Qasim Khundmiri <92255662+aliqasim54321@users.noreply.github.com>
2026-09-15 13:05:38 -0400

    Remove OpenAI API key for security reasons
    Removed sensitive OpenAI API key from _keys.py.

--- _keys.py
+++ _keys.py
@@ -1,5 +1,5 @@
 # https://platform.openai.com/settings/organization/api-keys
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = " "
 
 # https://portal.azure.com/#@lognpacific.com/resource/subscriptions/3c95e63a-895a-4386-991e-edbbf57de5c8/resourceGroups/Cyber-Range-Admin-SOC/providers/Microsoft.OperationalInsights/workspaces/LAW-Cyber-Range/Overview
 LOG_ANALYTICS_WORKSPACE_ID = "60c7f53e-249a-4077-b68e-55a4ae877d7c"
